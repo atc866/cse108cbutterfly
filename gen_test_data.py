d420efc8fae4e2d2a5a44c176e823cfdbe8efede
Sayer Harry <sayerh2005@gmail.com>
2025-03-16 20:44:56 -0700

    updated test file generator

--- gen_test_data.py
+++ gen_test_data.py
@@ -16,18 +16,12 @@ def gen_data(name, count):
 def gen_bucket_data(name, count, size):
     indexes = list(range(0, count))
     file = open(name, "w")
-    file.write("[")
     for i in range(count):
         random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=size))
         random_index = random.choice(indexes)
         indexes.remove(random_index)
         random_string.replace(",", "")
-        file.write('("Sort_value": ')
-        file.write(str(random_index))
-        file.write(', "Payload": "')
-        file.write(random_string)
-        file.write('")')
-    file.write("]")
+        file.write('{"sort_value": %d, "payload": %s},' %(random_index, random_string))
     file.close()
         
 gen_bucket_data("bstrings17.json", 2**17, 5)
--- test_bitonic_sort.cpp
+++ test_bitonic_sort.cpp
@@ -1,20 +1,26 @@
 #include "bitonic_sort.h"
 #include <iostream>
+#include <fstream>
 #include <random>
-
+#include <cstring>
 
 int main(int argc, char** argv) {
-    //take argument as input data size, only powers of 2
+    //take arguments 1 of file name, 2 as input size
 
     //generate input data
     vector<string> input_data;
-    int input_size
+    int input_size = stoi(argv[2]);
+    ifstream file(argv[1]); 
+    string op;
+    while(getline(file, op, ',').good()) {
+        input_data.emplace_back(op);
+    }
 
-    Server server(input_data, 16);
+    Server server(input_data, input_size);
     Client client(&server);
 
     try {
-        client.sort(8, 1);
+        client.sort(input_size, 1);
 
     }
     catch (const exception &ex) {
