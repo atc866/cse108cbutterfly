ed11c7aec798a292e34a859bbfe48bc793e86c24
Sayer Harry <sayerh2005@gmail.com>
2025-03-21 13:23:44 -0700

    cleaned git and added readme

--- gen_test_data.py
+++ gen_test_data.py
@@ -17,12 +17,14 @@ def gen_bucket_data(name, count, size):
     indexes = list(range(0, count))
     file = open(name, "w")
     file.write("[\n")
-    for i in range(count):
+    file.write('  {"sort_value": %d, "payload": "%s"}' %(random.randint(0, count), ''.join(random.choices(string.ascii_letters + string.digits, k=size))))
+    for i in range(1, count):
+        file.write(",\n")
         random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=size))
         random_index = random.randint(0, count)
         random_string.replace(",", "")
-        file.write('  {"sort_value": %d, "payload": "%s"},\n' %(random_index, random_string))
-    file.write("]")
+        file.write('  {"sort_value": %d, "payload": "%s"}' %(random_index, random_string))
+    file.write("\n]")
     file.close()
         
 gen_bucket_data("bstrings22.json", 2**22, 4)
